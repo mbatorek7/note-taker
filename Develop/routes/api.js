6b93d247b3b07e5cbaf7a1a806babadd268ee07a
mbatorek7 <maegan.batorek@valpo.edu>
2023-03-20 20:23:32 -0500

    minor bugs

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -19,30 +19,23 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-    //read file
     let db = fs.readFileSync("./db/db.json");
     db = JSON.parse(db);
-    res.json(db);
-    //take contents of file
-    //add my body data and id
     var newNote = {
         title: req.body.title,
         text: req.body.text,
         // creating unique id for each note
         id: uniqid(),
-      };
-    //write file with new array
+    };
     db.push(newNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(db));
     res.json(db);
 });
 
 router.delete('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    //read file
     const db = JSON.parse(fs.readFileSync("./db/db.json"));
     //collect everything except note with ID we want to delete
-    const deleteNote = db.filter(item => item.id !== id);
+    const deleteNote = db.filter(item => item.id !== req.params.id);
     // update file based on note we took out
     fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
     res.json(deleteNote);
